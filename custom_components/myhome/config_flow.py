fe81a001e2e99501c4447cc3b88310d3098ddde4
Matteo Mantovanelli <matteo.mantovanelli@gmail.com>
2026-07-07 23:27:29 +0200

    Fix AttributeError property config_entry of OptionsFlow object has no setter (HA 2025.12+ compatibility)

--- custom_components/myhome/config_flow.py
+++ custom_components/myhome/config_flow.py
@@ -377,7 +377,7 @@ class MyhomeOptionsFlowHandler(OptionsFlow):
 
     def __init__(self, config_entry):
         """Initialize MyHome options flow."""
-        self.config_entry = config_entry
+        super().__init__(config_entry)
         self.options = dict(config_entry.options)
         self.data = dict(config_entry.data)
         if CONF_WORKER_COUNT not in self.options:
